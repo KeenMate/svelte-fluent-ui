8109cb9a8546fd2fcbf8a85344e7b4edfcf31252
Filip Jakab <jakabfilip.fj@gmail.com>
2025-05-21 21:45:20 +0200

    fix: removed remnants of previous @fluentui/web-components@3.0.0's setup code

--- src/lib/fluent-ui/setup.ts
+++ src/lib/fluent-ui/setup.ts
@@ -1,6 +1,6 @@
-import {setTheme} from "@fluentui/web-components"
-import {webLightTheme} from "@fluentui/tokens"
-
-// Fluent UI Web Components are styled using tokens in the form of CSS variables.
-// You can use the setTheme utility to provide a theme for your website or application.
-setTheme(webLightTheme)
+// import {setTheme} from "@fluentui/web-components"
+// import {webLightTheme} from "@fluentui/tokens"
+//
+// // Fluent UI Web Components are styled using tokens in the form of CSS variables.
+// // You can use the setTheme utility to provide a theme for your website or application.
+// setTheme(webLightTheme)
