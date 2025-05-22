94bcdd1c2f6e993eb4b996de26e420a46e042f79
Ondrej Valenta <ondrej.valenta@keenmate.com>
2025-05-22 02:31:00 +0200

    - new list of components on main page to track updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,225 @@
 	let dialogElement: any
 </script>
 
-<h1>Fluent UI Svelte – Full Component Demo</h1>
+
+<div class="pure-g">
+		<div class="pure-u-1 pure-u-xl-1-2 pure-u-xxl-1-3 pure-u-xxxl-1-4">
+			<h1>List of components and their state</h1>
+
+<table class="pure-table pure-table-striped">
+	<thead>
+		<tr>
+			<th>State</th>
+			<th>Name</th>
+			<th>Comment</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+	<td></td>
+	<td>accordion</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>anchor</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>anchored-region</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>badge</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>breadcrumb</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>breadcrumb-item</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>button</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>calendar</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>card</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>checkbox</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>color</td>
+	<td></td>
+	</tr>
+<tr>
+	<td>Initial tests</td>
+	<td>combobox</td>
+	<td>Disable option is displayed as disabled, but can still be selected</td>
+	</tr>
+<tr>
+	<td></td>
+	<td>data-grid</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>design-system-provider</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>dialog</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>divider</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>flipper</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>horizontal-scroll</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>listbox</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>listbox-option</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>menu</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>menu-item</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>number-field</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>progress</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>radio</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>radio-group</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>search</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>select</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>skeleton</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>slider</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>slider-label</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>styles</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>switch</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>tabs</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>text-area</td>
+	<td></td>
+	</tr>
+<tr>
+	<td>Initial tests</td>
+	<td>text-field</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>toolbar</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>tooltip</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>tree-item</td>
+	<td></td>
+	</tr>
+<tr>
+	<td></td>
+	<td>tree-view</td>
+	<td></td>
+	</tr>
+	</tbody>
+</table>
+		</div>
+		<div class="pure-u-1 pure-u-xl-1-2 pure-u-xxl-1-3 pure-u-xxxl-1-4">
+			<h1>Fluent UI Svelte – Full Component Demo</h1>
 
 <!-- Example Section -->
 <div class="components">
@@ -125,7 +343,16 @@
 		</div>
 	</div>
 </div>
+		</div>
+</div>
+
 
 <!--<style lang="scss">-->
 <!--	@import ;-->
 <!--</style>-->
+
+
+
+
+
+
