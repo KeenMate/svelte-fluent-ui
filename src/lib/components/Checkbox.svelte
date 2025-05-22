1aef6fe97670615873ae07cee4d95f69781a7952
Filip Jakab <jakabfilip.fj@gmail.com>
2025-05-23 00:55:42 +0200

    fix: some props incorrectly required
    And tried to fix intermediate state

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -6,31 +6,66 @@
 	)
 
 	type Props = {
-		autofocus: boolean;
-		checked: boolean;
+		checked: boolean | null;
+		withIntermediate?: boolean;
+		autofocus?: boolean;
 		children?: any;
 		disabled?: boolean;
-		required: boolean;
-		value: string;
+		readonly?: boolean;
+		required?: boolean;
 
 		checkValidity?: () => boolean;
 		reportValidity?: () => boolean;
-		setCustomValidity: (message: string) => any;
+		setCustomValidity?: (message: string) => any;
 		setValidity?: (flags: any, message: any, anchor: any) => void;
+		onClick?: (ev: PointerEvent, previousValue: boolean | null) => void
 	}
 
 	let {
 		    checked  = $bindable(),
+		    withIntermediate  = undefined,
 		    disabled = undefined,
-		    children = undefined
+		    readonly = undefined,
+		    children = undefined,
+		onClick = undefined
 	    }: Props = $props()
+
+	let intermediate = $state(false)
+
+	function handleOnClick(ev: PointerEvent) {
+		ev.preventDefault()
+		ev.stopImmediatePropagation()
+
+		if (readonly || disabled) {
+			return
+		}
+
+		const previousValue = $state.snapshot(checked)
+		// intermediate state not working
+		// const newChecked = (ev as any).target.checked
+		// if (withIntermediate) {
+		// 	if (previousValue && !intermediate) {
+		// 		console.log("setting intermediate")
+		// 		intermediate = true
+		// 	} else {
+		// 		console.log("regular update checked")
+		// 		checked = newChecked
+		// 		intermediate = false
+		// 	}
+		// } else {
+		// 	checked = newChecked
+		// }
+		checked = (ev as any).target.checked
+
+		onClick?.(ev, previousValue)
+	}
 </script>
 
 <fluent-checkbox
 	{checked}
+	{readonly}
 	{disabled}
-	oncheckedchanged={ev => checked = ev.checked}
+	onclick={handleOnClick}
 >
-	<!-- todo: finish Checkbox -->
 	{@render children?.()}
 </fluent-checkbox>
