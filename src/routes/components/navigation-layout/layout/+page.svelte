d1088e23a5027e51bd974fea389875ed792d9e81
Filip Jakab <filip.jakab@keenmate.com>
2025-06-03 15:09:35 +0200

    feat: Layout component example

---
+++ src/routes/components/navigation-layout/layout/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import {Layout} from "$lib/index.js"
+</script>
+
+<div class="header">
+	<h1>Layout</h1>
+</div>
+
+<div class="content">
+	<div class="pure-g">
+		<div class="pure-u-1 pure-u-xl-1-2 pure-u-xxl-1-3 pure-u-xxxl-1-4">
+			<h2>Members</h2>
+			<table class="pure-table member-table">
+				<tbody>
+				<tr class="property">
+					<td colspan="5">Members</td>
+				</tr>
+				<!--<tr class="property">-->
+				<!--	<td></td>-->
+				<!--	<td>appearance</td>-->
+				<!--	<td>boolean</td>-->
+				<!--	<td>undefined</td>-->
+				<!--	<td></td>-->
+				<!--</tr>-->
+
+				<tr class="callback">
+					<td colspan="5">Callbacks</td>
+				</tr>
+				<!--<tr class="callback">-->
+				<!--	<td></td>-->
+				<!--	<td>checkValidity</td>-->
+				<!--	<td>() => boolean</td>-->
+				<!--	<td>undefined</td>-->
+				<!--	<td></td>-->
+				<!--</tr>-->
+
+				<tr class="slot">
+					<td colspan="5">Slots</td>
+				</tr>
+				<tr class="slot">
+					<td></td>
+					<td>children</td>
+					<td>any</td>
+					<td>undefined</td>
+					<td></td>
+				</tr>
+				</tbody>
+			</table>
+
+		</div>
+		<div class="pure-u-1 pure-u-xl-1-2 pure-u-xxl-1-3 pure-u-xxxl-1-4">
+			<h2 class="content-subhead">Actions</h2>
+			<table class="pure-table member-table">
+				<tbody>
+				<tr class="action">
+					<td colspan="4">Actions</td>
+				</tr>
+				<!--<tr class="action">-->
+				<!--	<td></td>-->
+				<!--	<td>select</td>-->
+				<!--	<td>() => void;</td>-->
+				<!--	<td></td>-->
+				<!--</tr>-->
+				</tbody>
+			</table>
+		</div>
+	</div>
+
+	<h2 class="content-subhead">Examples</h2>
+	<p>
+		<Layout>
+			<p>
+				Inner content of layout
+			</p>
+		</Layout>
+	</p>
+</div>
